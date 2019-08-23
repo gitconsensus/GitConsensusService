4266d478e3c96d503c2fd3e27b5db2b2af00ae3c
Robert Hafner <tedivm@tedivm.com>
2019-08-22 22:40:37 -0700

    Add print statement to root task setup

--- gitconsensusservice/worker.py
+++ gitconsensusservice/worker.py
@@ -5,6 +5,7 @@ from gitconsensusservice import celery, app
 
 @celery.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
+    print('Root Task - Schedule Installation Jobs')
     sender.add_periodic_task(
         float(app.config.get('PROCESS_INSTALLS_INTERVAL', 5 * 60.0)),
         consensus.process_installs.s(),
